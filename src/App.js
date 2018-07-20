28e14e099631822ffee1dc47c3d55ba0464542c0
Alina Myunster <amyunster@Alina-Myunsters-MacBookPro.local>
2018-07-20 15:19:11 -0700

    conditional statment added

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
       { name: "Alina", age: 30 },
       { name: "Jonathan", age: 3 },
       { name: "Leah", age: 1 },
-    ]
+    ],
+    showPerson: false
   };
 
   switchNameHandler = (newName) => {
@@ -31,6 +32,13 @@ class App extends Component {
     })
   }
 
+  tooglePersonHandler = () => {
+    let doesShow = this.state.showPerson;
+    this.setState({
+      showPerson: !doesShow
+    })
+  }
+
   render() {
     const style = {
       backgroundColor: 'white',
@@ -38,26 +46,36 @@ class App extends Component {
       border: '1px solid blue',
       padding: '8px',
       cursor: 'pointer'
-    }
+    };
+
+    let persons = null;
+    if (this.state.showPerson) {
+      persons = (
+        <div>
+          <Person
+            name={this.state.person[0].name}
+            age={this.state.person[0].age} />
+          <Person
+            name={this.state.person[1].name}
+            age={this.state.person[1].age}
+            click={this.switchNameHandler.bind(this, "Alina Myunster")}
+            changed={this.nameChangedHandler}>
+            My hobbie: tennis</Person>
+          <Person
+            name={this.state.person[2].name}
+            age={this.state.person[2].age} />
+        </div>
+      );
+    };
+
     return (
       <div className="App">
         <h1> Hi, I'm a react app</h1>
         <p> This is realy working </p>
-        <button 
-        style ={style}
-        onClick={() => this.switchNameHandler("Alina M")}> switch the name </button>
-        <Person
-          name={this.state.person[0].name}
-          age={this.state.person[0].age} />
-        <Person
-          name={this.state.person[1].name}
-          age={this.state.person[1].age}
-          click={this.switchNameHandler.bind(this, "Alina Myunster")}
-          changed={this.nameChangedHandler}> 
-          My hobbie: tennis</Person>
-        <Person
-          name={this.state.person[2].name}
-          age={this.state.person[2].age} />
+        <button
+          style={style}
+          onClick={this.tooglePersonHandler}> toogle persons </button>
+        {persons}
       </div>
     );
   }
